1c07c98d2c004526278f2a95259d33c6b5465b6f
Aday-Monzon <Aday-Monzon@users.noreply.github.com>
2021-09-26 00:12:56 +0100

    diseño del index

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,6 +22,7 @@
         
         <!-- Styles -->
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lexend+Giga:wght@500&display=swap" rel="stylesheet">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/fontawesome-all.min.css" rel="stylesheet">
         <link href="css/swiper.css" rel="stylesheet">
@@ -38,9 +39,9 @@
 
                 <!-- Image Logo -->
                 
-                <a class="navbar-brand logo-text" href="index.html">Enaclas</a>
+                <a class="navbar-brand logo-text" href="index.html" style="font-family: 'Lexend Giga', sans-serif;">ENACLAS</a>
                 <!-- Text Logo - Use this if you don't have a graphic logo -->
-                <!-- <a class="navbar-brand logo-image" href="index.html"><img src="images/logo.svg" alt="alternative"></a>  -->
+                <!-- <a class="navbar-brand logo-image" href="index.html"><img src="images/1sinfondo.png" alt="alternative"></a>  -->
 
                 <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -52,15 +53,18 @@
                             <a class="nav-link active" aria-current="page" href="#header">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#features">Features</a>
+                            <a class="nav-link" href="#features">Servicios</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#details">Details</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="#contacto">Contacto</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="#pricing">Precios</a>
                         </li>
-                        <li class="nav-item dropdown">
+                        {{-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Drop</a>
                             <ul class="dropdown-menu" aria-labelledby="dropdown01">
                                 <li><a class="dropdown-item" href="article.html">Article Details</a></li>
@@ -69,11 +73,11 @@
                                 <li><div class="dropdown-divider"></div></li>
                                 <li><a class="dropdown-item" href="privacy.html">Privacy Policy</a></li>
                             </ul>
-                        </li>
+                        </li> --}}
                     </ul>
-                    {{-- <span class="nav-item">
-                        <a class="btn-outline-sm" href="log-in.html">Log in</a>
-                    </span> --}}
+                    <span class="nav-item">
+                        <a class="btn-outline-sm" href="#">Aula Virtual</a>
+                    </span>
                 </div> <!-- end of navbar-collapse -->
             </div> <!-- end of container -->
         </nav> <!-- end of navbar -->
@@ -86,14 +90,14 @@
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="text-container">
-                            <h1 class="h1-large">Centro de Estudios <span class="replace-me">Enaclas, Enaclas, Enaclas</span></h1>
+                            <h1 class="h1-large">Centro de Estudios <span style="font-family: 'Lexend Giga', sans-serif;" class="replace-me">enaclas</span></h1>
                             <p class="p-large">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim, neque ut vanic barem ultrices sollicitudin</p>
                             {{-- <a class="btn-solid-lg" href="sign-up.html">Sign up for free</a> --}}
                         </div> <!-- end of text-container -->
                     </div> <!-- end of col -->
                     <div class="col-lg-6">
                         <div class="image-container">
-                            <img class="img-fluid" src="images/ReadingBooks.png" alt="alternative">
+                            <img class="img-fluid" src="images/sinfondo.png" alt="alternative">
                         </div> <!-- end of image-container -->
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
@@ -162,21 +166,20 @@
                 <div class="row">
                     <div class="col-lg-6 col-xl-5">
                         <div class="text-container">
-                            <h2>Gestiona las expectativas de tus alumnos y haz que confíen en ti</h2>
+                            <h2>Mejoramos las expectativas de nuestros alumnos y hacemos que confíen en lograr sus objetivos.</h2>
                             <p>Vestibulum ullamcorper augue ex, imperdiet tincidunt tellus bibendum inconsectetur rutrum mauris orbi scelerisque cursus augue, ac suscipit sem mattis at ut suscipit</p>
-                            <a class="btn-solid-reg" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Modal</a>
+                           
                         </div> <!-- end of text-container -->
                     </div> <!-- end of col -->
                     <div class="col-lg-6 col-xl-7">
                         <div class="image-container">
-                            <img class="img-fluid" src="images/details-1.svg" alt="alternative">
+                            <img class="img-fluid" src="images/undraw_exams_g4ow.svg" alt="alternative">
                         </div> <!-- end of image-container -->
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
             </div> <!-- end of container -->
         </div> <!-- end of basic-1 -->
         <!-- end of details 1 -->
-
 
         <!-- Details Modal -->
         <div id="staticBackdrop" class="modal fade" tabindex="-1" aria-hidden="true">
@@ -186,7 +189,7 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         <div class="col-lg-8">
                             <div class="image-container">
-                                <img class="img-fluid" src="images/details-modal.jpg" alt="alternative">
+                                <img class="img-fluid" src="images/table.png" alt="alternative">
                             </div> <!-- end of image-container -->
                         </div> <!-- end of col -->
                         <div class="col-lg-4">
@@ -230,10 +233,10 @@
         <!-- Details 2 -->
         <div class="basic-2">
             <div class="container">
-                <div class="row">
+                <div class="row align-items-lg-end">
                     <div class="col-lg-6">
                         <div class="image-container">
-                            <img class="img-fluid" src="images/details-2.png" alt="alternative">
+                            <img class="img-fluid" src="images/Clip.gif" alt="alternative">
                         </div> <!-- end of image-container -->
                     </div> <!-- end of col -->
                     <div class="col-lg-6">
@@ -254,6 +257,7 @@
                                     <div class="flex-grow-1">Quisque blandit pulvinar metus, eu sta malesuada</div>
                                 </li>
                             </ul>
+                             <a class="btn-solid-reg" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Modal</a>
                         </div> <!-- end of text-container -->
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
@@ -369,14 +373,82 @@
 
 
         <!-- Invitation -->
-        <div class="basic-3">
+        <div class="basic-3" id="contacto">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h4>Cualquiera puede empezar a utilizar la aplicación con un mínimo de conocimientos</h4>
+                        <h4 class="h2">Contacto</h4>
                         {{-- <a class="btn-outline-lg page-scroll" href="sign-up.html">Sign up for free</a> --}}
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
+                <div class="contact2"  id="contact">
+        
+  <div class="container">
+    <div class="row contact-container">
+      <div class="col-lg-12">
+        <div class="card card-shadow border-0 mb-4">
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="contact-box p-4">
+                <h4 class="title">Déjanos un mensaje</h4>
+                <form>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="nombre">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="email">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="movil">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="asunto">
+                      </div>
+                    </div>
+                    <div class="col-lg-12">
+                      <div class="form-group mt-3">
+                        <textarea class="form-control" rows="6"></textarea>
+                      </div>
+                    </div>
+                    <div class="col-lg-12">
+                      <button type="submit" class="btn btn-danger-gradiant mt-3 mb-3 text-white border-0 py-2 px-3"><span> ENVIAR <i class="ti-arrow-right"></i></span></button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-lg-4 bg-image" style="background-image:url(https://cdn.pixabay.com/photo/2015/12/01/15/43/black-1072366_960_720.jpg)">
+              <div class="detail-box p-4">
+                <h5 class="text-white font-weight-light mb-3">DIRECCIÓN</h5>
+                <p class="text-white op-7">C/ Isla de el Hierro <br> 35217 - Valsequillo<br> Las Palmas de GC
+                   </p>
+                <h5 class=" text-white font-weight-light mb-3 mt-4">TELEFONO</h5>
+                <p class=" text-white op-7">662 27 85 83
+                  <br> 928 016 749</p>
+                <h5 class=" text-white font-weight-light mb-3 mt-4">EMAIL</h5>
+                <p class=" text-white op-7">estudioenaclas@gmail.com
+                  </p>
+                <div class="round-social light">
+                  <a href="#" class="ml-0 text-decoration-none text-white border border-white rounded-circle"><i class="fab fa-facebook-square"></i></i></a>
+                  <a href="#" class="text-decoration-none text-white border border-white rounded-circle"><i class="fab fa-twitter-square"></i></a>
+                  <a href="#" class="text-decoration-none text-white border border-white rounded-circle"><i class="fab fa-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
             </div> <!-- end of container -->
         </div> <!-- end of basic-3 -->
         <!-- end of invitation -->
@@ -387,7 +459,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h2 class="h2-heading">Planes</h2>
+                        <h2 class="h2-heading">Precios</h2>
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
                 <div class="row">
@@ -397,7 +469,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="card-title">
-                                    <img class="decoration-lines" src="images/decoration-lines.svg" alt="alternative"><span>Free tier</span><img class="decoration-lines flipped" src="images/decoration-lines.svg" alt="alternative">
+                                    <img class="decoration-lines" src="images/decoration-lines.svg" alt="alternative"><span>Individual</span><img class="decoration-lines flipped" src="images/decoration-lines.svg" alt="alternative">
                                 </div>
                                 <ul class="list-unstyled li-space-lg">
                                     <li>Fusce pulvinar eu mi acm</li>
@@ -408,7 +480,7 @@
                                     <li>Vivamus dignissim sit amet</li>
                                     <li>Ut convallis aliquama set</li>
                                 </ul>
-                                <div class="price">Free</div>
+                                <div class="price">15€</div>
                                 <a href="sign-up.html" class="btn-solid-reg">Sign up</a>
                             </div>
                         </div>
@@ -418,7 +490,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="card-title">
-                                    <img class="decoration-lines" src="images/decoration-lines.svg" alt="alternative"><span>Advanced</span><img class="decoration-lines flipped" src="images/decoration-lines.svg" alt="alternative">
+                                    <img class="decoration-lines" src="images/decoration-lines.svg" alt="alternative"><span>Mensual</span><img class="decoration-lines flipped" src="images/decoration-lines.svg" alt="alternative">
                                 </div>
                                 <ul class="list-unstyled li-space-lg">
                                     <li>Nunc commodo magna quis</li>
@@ -429,7 +501,7 @@
                                     <li>Sed laoreet blandit mollis</li>
                                     <li>Mauris non luctus est</li>
                                 </ul>
-                                <div class="price">$19<span>/month</span></div>
+                                <div class="price">160€<span>/mes</span></div>
                                 <a href="sign-up.html" class="btn-solid-reg">Sign up</a>
                             </div>
                         </div>
@@ -439,7 +511,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="card-title">
-                                    <img class="decoration-lines" src="images/decoration-lines.svg" alt="alternative"><span>Professional</span><img class="decoration-lines flipped" src="images/decoration-lines.svg" alt="alternative">
+                                    <img class="decoration-lines" src="images/decoration-lines.svg" alt="alternative"><span>Bono 10</span><img class="decoration-lines flipped" src="images/decoration-lines.svg" alt="alternative">
                                 </div>
                                 <ul class="list-unstyled li-space-lg">
                                     <li>Quisque rutrum mattis</li>
@@ -450,7 +522,7 @@
                                     <li>Mollis felis. Integer id quam</li>
                                     <li>Id tellus hendrerit lacinia</li>
                                 </ul>
-                                <div class="price">$29<span>/month</span></div>
+                                <div class="price">75€<span>/month</span></div>
                                 <a href="sign-up.html" class="btn-solid-reg">Sign up</a>
                             </div>
                         </div>
@@ -461,7 +533,6 @@
             </div> <!-- end of container -->
         </div> <!-- end of cards-2 -->
         <!-- end of pricing -->
-
 
         <!-- Questions -->
         <div class="accordion-1">
@@ -514,15 +585,13 @@
             </div> <!-- end of container -->
         </div> <!-- end of accordion-1 -->
         <!-- end of questions -->
-
-
         <!-- Footer -->
         <div class="footer">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="footer-col first">
-                            <h6>About Website</h6>
+                            <h6>Contacto</h6>
                             <p class="p-small">Proin ut felis purus vestibulum in orci molestie, efficitur lacus ac pellentesque elit. Fusce mollis laoreet lobortis nulla ac efficitur lacus ac </p>
                         </div> <!-- end of footer-col -->
                         <div class="footer-col second">
